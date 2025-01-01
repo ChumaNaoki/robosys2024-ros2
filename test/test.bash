e55aacbbd960353c88958bb8cb47172f46850da8
Chuma Naoki <s23C1091UY@s.chibakoudai.jp>
2025-01-02 01:23:04 +0900

    テスト更新２

--- test/test.bash
+++ test/test.bash
@@ -14,3 +14,35 @@ grep '12角形の内角の和は: 1800°'
 timeout 20 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 cat /tmp/mypkg.log |
 grep '22角形の内角の和は: 3600°'
+
+timeout 30 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+cat /tmp/mypkg.log |
+grep '32角形の内角の和は: 5400°'
+
+timeout 40 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+cat /tmp/mypkg.log |
+grep '42角形の内角の和は: 7200°'
+
+timeout 50 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+cat /tmp/mypkg.log |
+grep '52角形の内角の和は: 9000°'
+
+timeout 60 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+cat /tmp/mypkg.log |
+grep '62角形の内角の和は: 10800°'
+
+timeout 70 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+cat /tmp/mypkg.log |
+grep '72角形の内角の和は: 12600°'
+
+timeout 80 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+cat /tmp/mypkg.log |
+grep '82角形の内角の和は: 14400°'
+
+timeout 90 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+cat /tmp/mypkg.log |
+grep '92角形の内角の和は: 16200°'
+
+timeout 100 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+cat /tmp/mypkg.log |
+grep '102角形の内角の和は: 18000°'

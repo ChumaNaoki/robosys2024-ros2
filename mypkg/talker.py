284566b73abc0effbeede3e8a82d9a456311b2e1
Chuma Naoki <s23C1091UY@s.chibakoudai.jp>
2025-01-02 01:02:48 +0900

    内角測定

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -4,17 +4,29 @@ from std_msgs.msg import Int16
 
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Int16, "countup" , 10)
-n = 0
 
-def cb():
+pub_countup = node.create_publisher(Int16, "countup", 10)
+pub_naikaku = node.create_publisher(Int16, "naikaku", 10)
+
+n = 0
+i = 3
+
+def cb(): #countup
+    global i
+    msg = Int16()
+    msg.data = i
+    pub_countup.publish(msg)
+
+def nakami(): #naikaku
     global n
+    global i
+    n = 180 * (i - 2)
+    i += 1
     msg = Int16()
     msg.data = n
-    pub.publish(msg)
-    n += 2
-    n *= 2 #listenerに渡す数値の操作
+    pub_naikaku.publish(msg)
 
 def main():
     node.create_timer(0.5, cb)
+    node.create_timer(0.5, nakami)
     rclpy.spin(node)

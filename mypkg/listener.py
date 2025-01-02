c33dacd77ae729ac569d0086138a5048dd639ab1
Chuma Naoki <s23C1091UY@s.chibakoudai.jp>
2025-01-03 02:52:16 +0900

    test

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,39 +1,13 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from custom_msgs.msg import ShapeInfo
 
 rclpy.init()
 node = Node("listener")
 
-# データを保存する変数
-current_shape = None
-current_angle_sum = None
-last_output_shape = None  # 最後に出力した角形
-last_output_angle_sum = None  # 最後に出力した内角の和
-
-def cb(msg): #角の数
-    global current_shape
-    current_shape = msg.data
-    log_combined()
-
-def nakami(msg): #内角の和
-    global current_angle_sum
-    current_angle_sum = msg.data
-    log_combined()
-
-def log_combined(): #受信データ出力 
-    global current_shape, current_angle_sum
-    global last_output_shape, last_output_angle_sum
-
-    # 両データが変化したことを確認
-    if current_shape is not None and current_angle_sum is not None:
-        if current_shape != last_output_shape and current_angle_sum != last_output_angle_sum:
-            node.get_logger().info(f"{current_shape}角形の内角の和は: {current_angle_sum}°")
-            last_output_shape = current_shape
-            last_output_angle_sum = current_angle_sum
+def callback(msg):
+    node.get_logger().info(f"{msg.sides}角形の内角の和は: {msg.angle_sum}°")
 
 def main():
-    node.create_subscription(Int16, "countup", cb, 10)
-    node.create_subscription(Int16, "naikaku", nakami, 10)
-    
+    node.create_subscription(ShapeInfo, "shape_info", callback, 10)
     rclpy.spin(node)
--- test/test.bash
+++ test/test.bash
@@ -4,45 +4,11 @@ dir=~
 [ "$1" != "" ] && dir="$1"
 
 cd $dir/ros2_ws
+rm -rf build install log
+colcon build --packages-select mypkg
 colcon build
 source $dir/.bashrc
-
+ls /root/ros2_ws/src/mypkg
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 cat /tmp/mypkg.log |
-grep '12角形の内角の和は: 1800°'
-
-timeout 20 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
-cat /tmp/mypkg.log |
-grep '22角形の内角の和は: 3600°'
-
-timeout 30 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
-cat /tmp/mypkg.log |
-grep '32角形の内角の和は: 5400°'
-
-timeout 40 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
-cat /tmp/mypkg.log |
-grep '42角形の内角の和は: 7200°'
-
-timeout 50 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
-cat /tmp/mypkg.log |
-grep '52角形の内角の和は: 9000°'
-
-timeout 60 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
-cat /tmp/mypkg.log |
-grep '62角形の内角の和は: 10800°'
-
-timeout 70 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
-cat /tmp/mypkg.log |
-grep '72角形の内角の和は: 12600°'
-
-timeout 80 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
-cat /tmp/mypkg.log |
-grep '82角形の内角の和は: 14400°'
-
-timeout 90 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
-cat /tmp/mypkg.log |
-grep '92角形の内角の和は: 16200°'
-
-timeout 100 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
-cat /tmp/mypkg.log |
-grep '102角形の内角の和は: 18000°'
+grep '3角形の内角の和は: 180°'

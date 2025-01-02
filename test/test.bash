5076cca9b02e05c8b995a236722f5ec857d1fa8a
Chuma Naoki <s23C1091UY@s.chibakoudai.jp>
2025-01-03 02:33:36 +0900

    ADD

--- test/test.bash
+++ test/test.bash
@@ -7,9 +7,8 @@ cd $dir/ros2_ws
 rm -rf build install log
 colcon build --packages-select mypkg
 colcon build
-source install/setup.bash
 source $dir/.bashrc
-source /root/ros2_ws/src/setup.bash
+source /root/ros2_ws/src/mypkg/setup.bash
 ls /root/ros2_ws/src/mypkg
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 cat /tmp/mypkg.log |

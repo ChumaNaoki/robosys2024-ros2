c266e90eb39283d9e7b2f6dec2eda03cc73a0759
Chuma Naoki <s23C1091UY@s.chibakoudai.jp>
2025-01-02 20:24:08 +0900

    test

--- test/test.bash
+++ test/test.bash
@@ -6,7 +6,7 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-timeout 9 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 cat /tmp/mypkg.log |
-grep '10角形の内角の和は: 1440°'
+grep '11角形の内角の和は: 1620°'

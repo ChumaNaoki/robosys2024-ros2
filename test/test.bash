8c3391775e42defd65e473a88d4e1ad007c47f1e
Chuma Naoki <s23C1091UY@s.chibakoudai.jp>
2025-01-03 01:44:17 +0900

    test

--- test/test.bash
+++ test/test.bash
@@ -7,7 +7,7 @@ cd $dir/ros2_ws
 colcon build
 source install/setup.bash
 source $dir/.bashrc
-ls /root/ros2_ws/install/mypkg
+ls /root/ros2_ws/src/mypkg
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 cat /tmp/mypkg.log |
 grep '3角形の内角の和は: 180°'

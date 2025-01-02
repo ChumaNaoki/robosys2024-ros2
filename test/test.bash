f40da964d7f245247298caeb692caf6a56cf05e5
Chuma Naoki <s23C1091UY@s.chibakoudai.jp>
2025-01-02 19:51:07 +0900

    test

--- test/test.bash
+++ test/test.bash
@@ -7,6 +7,6 @@ cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 
-timeout 9 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+timeout 8 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 cat /tmp/mypkg.log |
 grep '10角形の内角の和は: 1440°'

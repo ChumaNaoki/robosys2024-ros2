e8df44cc0db768c69979a03a56f2decbfccbab69
Chuma Naoki <s23C1091UY@s.chibakoudai.jp>
2025-01-03 01:20:13 +0900

    test実験

--- test/test.bash
+++ test/test.bash
@@ -7,4 +7,5 @@ cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
-cat /tmp/mypkg.log
+cat /tmp/mypkg.log |
+grep '12角形の内角の和は: 1800°'
